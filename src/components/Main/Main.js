88a0f4514c2bc69c62751c23279aa5d2f27a9f3a
Seokhyun Kim <seokhyunkim@seokhyuns-mbp.attlocal.net>
2020-06-05 00:57:29 -0500

    heroku deployed

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -26,7 +26,7 @@ export class Main extends Component {
     }
     axios
       .post(
-        "http://localhost:4000/search_result",
+        "/search_result",
         {
           keyword: this.state.keyword,
         },

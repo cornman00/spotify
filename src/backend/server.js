1aa2968f901159e3b284789a4da776e1a72bbd37
Seokhyun Kim <seokhyunkim@seokhyuns-mbp.attlocal.net>
2020-06-05 20:53:06 -0500

    favicon

--- src/backend/server.js
+++ src/backend/server.js
@@ -11,6 +11,9 @@ const port = process.env.PORT || 4000;
 require("dotenv").config();
 
 app.use(favicon(__dirname + "/build/favicon.ico"));
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "build")));
+
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
